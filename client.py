aa0ee51573cf0279a8491162a9b3212949134a86
abhinavxox <abhinavxox@gmail.com>
2023-06-17 13:45:17 +0530

    <Add>:[Gui code]

--- client.py
+++ client.py
@@ -1,18 +1,20 @@
+# client.py
+
 import socket
 import threading
+import tkinter as tk
 
 def receive_messages():
     while True:
         message = client_socket.recv(1024).decode()
-        print(message)
+        chat_box.insert(tk.END, message + '\n')
 
-def send_message():
-    while True:
-        message = input()
-        client_socket.send(message.encode())
-        if message == 'exit':
-            client_socket.close()
-            break
+def send_message(event=None):
+    message = input_box.get()
+    input_box.delete(0, tk.END)
+    client_socket.send(message.encode())
+    if message == 'exit':
+        client_socket.close()
 
 # Create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,9 +22,27 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect to the server's IP address and port number
 client_socket.connect(('localhost', 5555))  # Example: using localhost and port 5555
 
+# Create the GUI
+window = tk.Tk()
+window.title("Group Chat")
+
+# Create the chat box
+chat_box = tk.Text(window)
+chat_box.pack()
+
+# Create the input box
+input_box = tk.Entry(window)
+input_box.pack()
+
+# Set focus on the input box
+input_box.focus()
+
+# Bind the Enter key to send messages
+window.bind('<Return>', send_message)
+
 # Start receiving messages from the server
 receive_thread = threading.Thread(target=receive_messages)
 receive_thread.start()
 
-# Start sending messages to the server
-send_message()
+# Start the GUI main loop
+window.mainloop()
--- gui.py
+++ gui.py
@@ -1,30 +1,28 @@
-from tkinter import Tk, Text, Entry, Button, Scrollbar
+# gui.py
 
-class ChatApplication:
-    def __init__(self):
-        self.window = Tk()
-        self.window.title("Chat Window")
+import subprocess
+import tkinter as tk
 
-        self.chat_textbox = Text(self.window, height=20, width=50)
-        self.scrollbar = Scrollbar(self.window, command=self.chat_textbox.yview)
-        self.chat_textbox.configure(yscrollcommand=self.scrollbar.set)
-        self.input_entry = Entry(self.window)
-        self.send_button = Button(self.window, text="Send", command=self.send_message)
+def run_server():
+    subprocess.Popen(['python', 'server.py'])
 
-        self.chat_textbox.pack(side='top', padx=10, pady=10)
-        self.scrollbar.pack(side='right', fill='y')
-        self.input_entry.pack(side='left', padx=10)
-        self.send_button.pack(side='left')
+def run_client():
+    subprocess.Popen(['python', 'client.py'])
 
-    def send_message(self):
-        message = self.input_entry.get()
-        if message:
-            self.chat_textbox.insert('end', f"You: {message}\n")
-            self.input_entry.delete(0, 'end')
+# Create the GUI
+window = tk.Tk()
+window.title("Chat Application")
 
-    def run(self):
-        self.window.mainloop()
+# Create the server button
+server_button = tk.Button(window, text="Start Server", command=run_server)
+server_button.pack()
 
-if __name__ == "__main__":
-    app = ChatApplication()
-    app.run()
+# Create the client buttons
+client1_button = tk.Button(window, text="Start Client 1", command=run_client)
+client1_button.pack()
+
+client2_button = tk.Button(window, text="Start Client 2", command=run_client)
+client2_button.pack()
+
+# Start the GUI main loop
+window.mainloop()
